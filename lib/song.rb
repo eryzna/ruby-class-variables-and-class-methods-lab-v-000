931c0030516e8f271198cf8b1b659ea39af370ea
Elana Ryznar <eryzna@gmail.com>
2018-05-28 20:25:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   @@count=0
   @@genres=[]
+  @@artists=[]
   attr_accessor :name, :artist, :genre
   def initialize (name, artist, genre)
     @name=name
@@ -8,6 +9,7 @@ class Song
     @genre=genre
     @@count +=1
     @@genres << genre
+    @@artist<< artist
   end
   def self.count
     @@count
